72a08424968ea5646ebe5d3dc56d9423f96b183c
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-11-21 00:53:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("What time is it?")
 
 with st.form("my_form"):
-	youtube_url = st.text_input('Input YouTube URL')
+	youtube_url = st.text_input('Input YouTube URL', "https://www.youtube.com/playlist?list=PL1n2-n-o82sxNG4r2GyOQ7iX3Kg9KBz1s")
 
 # Every form must have a submit button.
 	submitted = st.form_submit_button("Submit")
