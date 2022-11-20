61c698a52a698ba45d8d9a999b1d4f429e2e81fe
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-11-21 00:42:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ model = whisper.load_model("base")
 # When mp4 file uploaded
 if uploaded_mp4_file is not None:
 	
-	st.write("Transcribing...")
+# 	st.write("Transcribing...")
 	
 # 	with NamedTemporaryFile(suffix="mp3") as temp:
 # 		st.write(uploaded_mp4_file.getvalue())
@@ -44,6 +44,8 @@ if uploaded_mp4_file is not None:
 	filename = pathlib.Path(uploaded_mp4_file.name).stem
 	
 	mp4_file_path = HERE / f'./{filename}_binaries.mp4'
+	
+	mp3_file_path = f'{filename}.mp3'
 
 	with open(mp4_file_path, 'wb') as binary_file:
 		video_bytes = uploaded_mp4_file.getvalue()
@@ -51,9 +53,12 @@ if uploaded_mp4_file is not None:
 		
 # 		st.write(mp4_file_path)
 		
-		mp4tomp3(str(mp4_file_path),"audio.mp3")
+		mp4tomp3(str(mp4_file_path), mp3_file_path)
+		
+		st.write("Transcribing...")
+		print(mp3_file_path)
 
-	transcription = model.transcribe("audio.mp3", language = 'en')
+	transcription = model.transcribe(mp3_file_path, language = 'en')
 
 	st.write(transcription['text'])	
 	st.write("Done with transcription!")
