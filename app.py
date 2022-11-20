7008bae4c57ba3cec9084dffa413af3c9b4a94bf
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-11-20 23:13:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ print(HERE)
 
 st.title("What Time is it?")
 
-uploaded_file = st.file_uploader("Upload Video File", accept_multiple_files=False)
+uploaded_file = st.file_uploader("Upload Video File", accept_multiple_files=True)
 
 model_size = 'tiny'
 
@@ -17,13 +17,21 @@ model = whisper.load_model(model_size)
 
 print("hello world")
 
-if uploaded_file is not None:
-	file_name = uploaded_file.name
-	print(file_name)
-	print("Transcribing...")
+# When mp4 file uploaded
+if uploaded_mp4_file is not None:
+	uploaded_mp4_file_length = len(uploaded_mp4_file.getvalue())
+
+	filename = pathlib.Path(uploaded_mp4_file.name).stem
 	
-# 	video_file = open(f"{HERE}/{file_name}", "wb")
-# 	video_file.write(uploaded_file.getbuffer())
-	
-	transcription = model.transcribe(f"{HERE}/{file_name}", language = 'en')
+	mp4_file_path = HERE / f'./{filename}_binaries.mp4'
+
+	with open(mp4_file_path, 'wb') as binary_file:
+		video_bytes = uploaded_mp4_file.getvalue()
+		binary_file.write(video_bytes)
+
+	with open(mp4_file_path, "rb") as file:
+		download_video_bytes = file.read() # read a byte (a single character in text)
+		# print(download_video_bytes)
+
+# 	transcription = model.transcribe(f"{HERE}/{file_name}", language = 'en')
 	print("Done with transcription!")
